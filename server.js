9e3a1b0fe2e191b59956a28bb2e436a3158011f7
Nireshkumar R <nireshkmr.raj@gmail.com>
2019-09-14 19:04:03 +0530

    PORT BUG FIXED

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const PORT =  process.env.port || 5001;
+const PORT =  process.env.PORT || 5001;
 
 app.use(express.static(path.resolve(__dirname,'build')))
 
